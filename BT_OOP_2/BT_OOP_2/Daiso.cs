4befac98e2ea364ac304cf2c964150c8d88b8ba6
AriTannia <ngophucnguyen1976@gmail.com>
2024-01-21 10:25:16 +0700

    BTVN OOP 21/01/24

---
+++ BT_OOP_2/BT_OOP_2/Daiso.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BT_OOP_2
+{
+    public abstract class Daiso
+    {
+        public abstract double Tinh();
+    }
+
+    public class SoPhuc : Daiso
+    {
+        private double phanthuc { get; set; }
+        private double phanao { get; set; }
+        public SoPhuc(double phanthuc, double phanao)
+        {
+            this.phanthuc = phanthuc;
+            this.phanao = phanao;
+        }
+
+        public override double Tinh()
+        {
+            double t1 = Math.Pow(phanthuc, 2) + Math.Pow(phanao, 2);
+            return Math.Round(Math.Sqrt(t1), 2);
+        }
+
+        public override string ToString()
+        {
+            string dau = (phanao > 0) ? "+" : "";
+            return $"Số phức: {phanthuc}{dau}{phanao}i";
+        }
+    }
+
+    public class Vector_2D : Daiso
+    {
+        public double x { get; set; }
+        public double y { get; set; }
+
+        public Vector_2D(double x, double y)
+        {
+            this.x = x;
+            this.y = y;
+        }
+
+        public override double Tinh()
+        {
+            double t1 = Math.Pow(-y, 2) + Math.Pow(x, 2);
+            return Math.Round(Math.Sqrt(t1), 2);
+        }
+
+        public override string ToString()
+        {
+            return $"Vector: ({x};{y})\nVector Vuông Góc: ({-y};{x})";
+        }
+    }
+}
---
+++ BT_OOP_2/BT_OOP_2/Program.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BT_OOP_2
+{
+    internal class Program
+    {
+        static List<Daiso> ds_spvt = new List<Daiso>();
+        static void Main(string[] args)
+        {
+            Console.InputEncoding = Encoding.Unicode;
+            Console.OutputEncoding = Encoding.Unicode;
+            Random rd1 = new Random();
+            for(int i = 0; i < 20; i++)
+            {
+                if(rd1.Next(0, 2) == 0)
+                {
+                    TaoSP(rd1.Next(-20, 21), rd1.Next(-20, 21));
+                }  
+                else
+                {
+                    TaoVector2D(rd1.Next(-20, 21), rd1.Next(-20, 21));
+                }    
+            }
+
+            int j = 1;
+            ds_spvt.ToList().ForEach(x =>
+            {
+                Console.WriteLine(new string('-', 50) + "\n");
+                Console.WriteLine($"{j++}/\n{x} >> Kết quả của phép tính {x.Tinh()}");
+                Console.WriteLine(new string('-', 50) + "\n");
+            });
+        }
+
+        static void TaoSP(double num1, double num2)
+        {
+            Random rd = new Random();
+            ds_spvt.Add(new SoPhuc(num1, num2));
+        }
+
+        static void TaoVector2D(double num1, double num2)
+        {
+            Random rd = new Random();
+            ds_spvt.Add(new Vector_2D(num1, num2));
+        }
+    }
+}
