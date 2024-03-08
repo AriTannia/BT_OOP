1eecd1e54fff840f46049b81b7b63d8643a4d587
AriTannia <ngophucnguyen1976@gmail.com>
2024-03-08 16:39:35 +0700

    BT OOP

---
+++ ConsoleApp1/ConsoleApp1/Print.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Text.Json;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    internal class Print
+    {
+        public static void Print_Write(string filepath, StudentList studentList)
+        {
+            try
+            {
+                string json = JsonSerializer.Serialize(studentList,
+    new JsonSerializerOptions { WriteIndented = true });
+                using (StreamWriter writer = new StreamWriter(filepath))
+                {
+                    writer.Write(json);
+                }
+            }
+            catch(Exception ex)
+            {
+                Console.WriteLine($"Error Message: {ex.ToString()}");
+            }
+        }
+
+        public static StudentList Print_Read(string filepath)
+        {
+            try
+            {
+                string json = String.Empty;
+                using (StreamReader reader = new StreamReader(filepath))
+                {
+                    json = reader.ReadToEnd();
+                }
+                StudentList new_stList = JsonSerializer.Deserialize<StudentList>(json);
+                return new_stList;
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine($"Error Message: {ex.ToString()}");
+            }
+            throw new ArgumentException("Error Write.");
+        }
+    }
+}
