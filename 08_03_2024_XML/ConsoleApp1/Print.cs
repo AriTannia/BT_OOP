5e231120665881172d6f2da0877fd33d689f5033
AriTannia <ngophucnguyen1976@gmail.com>
2024-03-08 17:15:44 +0700

    BT OOP XML

---
+++ 08_03_2024_XML/ConsoleApp1/Print.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Xml.Serialization;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    internal class Print
+    {
+        public static void Print_Write(string filepath, StudentList studentList)
+        {
+            try
+            {
+                using (FileStream stream = new FileStream(filepath, FileMode.Create))
+                {
+                    XmlSerializer xmlSerializer = new XmlSerializer(typeof(StudentList));
+                    xmlSerializer.Serialize(stream, studentList);
+                }
+            }
+            catch(Exception ex)
+            {
+                Console.WriteLine($"Error Message: {ex.ToString()}");
+            }
+        }
+
+        public static StudentList Print_Read(string filepath)
+        { 
+            try
+            {
+                StudentList new_stList;
+                using (FileStream stream = File.OpenRead(filepath))
+                {
+                    XmlSerializer xmlSerializer = new XmlSerializer(typeof(StudentList));
+                    new_stList = xmlSerializer.Deserialize(stream) as StudentList;
+                }
+                
+                return new_stList;
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine($"Error Message: {ex.ToString()}");
+            }
+            return null;
+        }
+    }
+}
---
+++ 08_03_2024_XML/ConsoleApp1/Program.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.IO;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            StudentList studentList = new StudentList();
+            studentList.Add(new Student { Id = 1, Name = "Nam", Age = 34 });
+            studentList.Add(new Student { Id = 2, Name = "Binh", Age = 35 });
+            studentList.Add(new Student { Id = 3, Name = "Minh", Age = 36 });
+
+            string filepath = "data.xml";
+            // Request 2
+            Print.Print_Write(filepath, studentList);
+            // Request 3
+            StudentList new_stList = Print.Print_Read(filepath);
+
+            //Read File Turn 1
+            foreach (Student student in new_stList.Students)
+            {
+                Console.WriteLine(student);
+            }
+            Console.Clear();
+
+            // Request 4
+            new_stList.Add(new Student { Id = 4, Name = "Hawk", Age = 31 });
+            new_stList.Add(new Student { Id = 5, Name = "Tom", Age = 25 });
+
+            // Request 5
+            Print.Print_Write(filepath, new_stList);
+            // Request 6
+            StudentList new_stList_2 = Print.Print_Read(filepath);
+
+            //Read File Turn 2 - Request 7
+            foreach(Student student in new_stList_2.Students)
+            {
+                Console.WriteLine(student);
+            }
+
+        }
+    }
+}
